5efb49fe43aeba45525c1d697752d2e26880b1dd
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-02 11:51:47 -0500

    Migrate SummarizedExperiment methods here

---
+++ R/AllGenerics.R
@@ -0,0 +1,6 @@
+#' @rdname coerce-SummarizedExperiment
+#' @name as.SummarizedExperiment
+#' @importFrom AcidGenerics as.SummarizedExperiment
+#' @usage as.SummarizedExperiment(x, ...)
+#' @export
+NULL
