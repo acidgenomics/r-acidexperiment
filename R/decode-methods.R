4abba6388765ac7020efb3aaeca97f674ad51d47
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-03 08:13:10 -0500

    Work toward resolving FIXMEs

--- R/decode-methods.R
+++ R/decode-methods.R
@@ -1,10 +1,8 @@
-## FIXME INHERIT THIS FROM ACIDGENERICS INSTEAD.
-
 #' Decode column data that uses run-length encoding
 #'
 #' @name decode
 #' @inherit S4Vectors::decode description return
-#' @note Updated 2021-02-02.
+#' @note Updated 2021-02-03.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @param ... Additional arguments.
@@ -16,18 +14,12 @@
 #'
 #' @examples
 #' data(RangedSummarizedExperiment, package = "AcidTest")
-#' rowData <- SummarizedExperiment::rowData
-#'
-#' ## DataFrame ====
-#' df <- rowData(RangedSummarizedExperiment)
-#' lapply(df, class)
-#' x <- decode(df)
-#' lapply(x, class)
 #'
 #' ## SummarizedExperiment ====
 #' ## This works on rowData and colData.
-#' x <- decode(RangedSummarizedExperiment)
-#' lapply(rowData(x), class)
+#' object <- RangedSummarizedExperiment
+#' object <- decode(object)
+#' lapply(rowData(object), class)
 NULL
 
 
