ee65f0a14cd4ae74a1a6293f86897f2e46875a75
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-03 09:42:59 -0500

    Simplify unit test

--- tests/testthat/test-organism.R
+++ tests/testthat/test-organism.R
@@ -1,37 +1,16 @@
 context("organism")
 
-object <- rse
-colnames(mcols(rowRanges(object))) <-
-    camelCase(
-        object = colnames(mcols(rowRanges(object))),
-        strict = TRUE
+test_that("SummarizedExperiment", {
+    object <- rse
+    expect_identical(
+        object = organism(object),
+        expected = "Homo sapiens"
     )
-rownames(object) <- as.character(rowData(object)[["geneId"]])
-
-## nolint start
-matrix <- assay(object)
-GRanges <- rowRanges(object)
-DataFrame <- as(as.data.frame(GRanges), "DataFrame")
-## nolint end
-
-test_that("organism", {
-    for (object in list(
-        "matrix" = matrix,
-        "DataFrame" = DataFrame,
-        "GRanges" = GRanges,
-        "SummarizedExperiment" = object
-    )) {
-        expect_identical(
-            object = organism(object),
-            expected = "Homo sapiens"
-        )
-    }
 })
 
 test_that("Metadata stash", {
+    object <- rse
     org <- "xxx"
     metadata(object)[["organism"]] <- org
     expect_identical(organism(object), org)
 })
-
-rm(object)
