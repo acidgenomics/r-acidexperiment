20b7efb3dd9b9024ff5ddc708a3c219c345abf41
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-03 08:13:10 -0500

    Work toward resolving FIXMEs

--- R/encode-methods.R
+++ R/encode-methods.R
@@ -1,6 +1,6 @@
 #' @name encode
 #' @inherit AcidGenerics::encode
-#' @note Updated 2021-02-02.
+#' @note Updated 2021-02-03.
 #'
 #' @inheritParams AcidRoxygen::params
 #' @param ... Additional arguments.
@@ -11,17 +11,13 @@
 #' All `atomic` columns will be encoded to `Rle` S4 class.
 #'
 #' @examples
-#' ## FIXME NEED RSE EXAMPLE HERE.
+#' data(RangedSummarizedExperiment, package = "AcidTest")
 #'
-#' ## DataFrame ====
-#' binary <- seq(from = 0L, to = 1L)
-#' df <- S4Vectors::DataFrame(
-#'     a = rep(x = binary, times = 50L),
-#'     b = rep(x = binary, each = 50L)
-#' )
-#' lapply(df, class)
-#' x <- encode(df)
-#' lapply(x, class)
+#' ## SummarizedExperiment ====
+#' ## This works on rowData and colData.
+#' object <- RangedSummarizedExperiment
+#' object <- encode(object)
+#' lapply(rowData(object), class)
 NULL
 
 
