02b712011ef42e0f91ff50b21ab65447cd912d3a
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-08 15:09:49 -0500

    Update NAMESPACE and work toward reexports

---
+++ R/reexports.R
@@ -0,0 +1,9 @@
+#' @importFrom SummarizedExperiment SummarizedExperiment assay assay<-
+#'   assayNames assayNames<- assays assays<- colData colData<- rowData rowData<-
+#'   rowRanges rowRanges<-
+NULL
+
+
+
+#' @importFrom sessioninfo session_info
+NULL
