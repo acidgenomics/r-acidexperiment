88910430b9f20d9f39b0948ae6f70b58520999d8
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-10 17:23:09 -0500

    goalie is now an s4 object not s3

--- tests/testthat/test-matchesInterestingGroups.R
+++ tests/testthat/test-matchesInterestingGroups.R
@@ -13,7 +13,7 @@ test_that("Allow 'NULL' to pass", {
 test_that("Match failure", {
     ok <- matchesInterestingGroups(rse, c("XXX", "YYY"))
     expect_false(ok)
-    expect_s3_class(ok, "goalie")
+    expect_s4_class(ok, "goalie")
     expect_identical(
         cause(ok),
         noquote("Interesting groups are not defined in 'sampleData()'.")
