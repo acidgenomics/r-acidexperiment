37b2b8273003d4980818a9be2d951871c5f729a9
Michael Steinbaugh <mike@steinbaugh.com>
2021-06-04 12:13:32 -0400

    Fix unit test

--- tests/testthat/test-convertGenesToSymbols.R
+++ tests/testthat/test-convertGenesToSymbols.R
@@ -66,7 +66,10 @@ test_that("matrix", {
             c("sample1", "sample2")
         )
     )
-    object <- convertGenesToSymbols(gene2symbol = gene2symbol)
+    object <- convertGenesToSymbols(
+        object = object,
+        gene2symbol = gene2symbol
+    )
     expect_identical(
         object = rownames(object),
         expected = c("TSPAN6", "TNMD")
