58451da07bd0c987f20136867f783f3b5b69e458
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-10 15:32:28 -0400

    Update unit tests

--- tests/testthat/test-Ensembl2Entrez.R
+++ tests/testthat/test-Ensembl2Entrez.R
@@ -1,11 +1,14 @@
 context("Ensembl2Entrez")
 
-formats <- eval(formals(`Ensembl2Entrez,RSE`)[["format"]])
 test_that("RangedSummarizedExperiment", {
+    formats <- eval(methodFormals(
+        f = "Ensembl2Entrez",
+        signature = "GRanges",
+        package = "AcidGenomes"
+    )[["format"]])
     for (format in formats) {
         object <- rse
         object <- Ensembl2Entrez(object = object, format = format)
         expect_s4_class(object, "Ensembl2Entrez")
     }
 })
-rm(formats)
