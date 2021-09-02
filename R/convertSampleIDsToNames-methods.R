5618f1841c2bb49cc9b6205c93bbb0fd8b376f21
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-02 12:16:35 -0400

    Improve code coverage

--- R/convertSampleIDsToNames-methods.R
+++ R/convertSampleIDsToNames-methods.R
@@ -15,28 +15,23 @@ NULL
 
 
 
-## NULL assignment into a column name doesn't work for DataFrame class.
-## You can see this cryptic error on some R installations:
-##
-## nolint start
-## > colData(object)[["sampleName"]] <- NULL
-## Error in replaceROWS(x, if (missing(i)) nsbs else i, value) :
-##   appending gaps is not supported
-## nolint end
-##
 ## Updated 2021-02-02.
 `convertSampleIDsToNames,SE` <-  # nolint
     function(object) {
         validObject(object)
         sampleNames <- sampleNames(object)
         if (
-            identical(as.character(sampleNames), colnames(object)) ||
-            !identical(names(sampleNames), colnames(object))
+            identical(
+                x = as.character(sampleNames),
+                y = colnames(object)
+            ) ||
+            !identical(
+                x = names(sampleNames),
+                y = colnames(object)
+            )
         ) {
-            ## nocov start  FIXME
             alertWarning("Returning object with sample names unmodified.")
             return(object)
-            ## nocov end  FIXME
         }
         colnames <- as.character(sampleNames)
         assert(hasNoDuplicates(colnames))
