6e6679d46b613f0337e6ace5c1dfde593b748e38
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-02 17:03:43 -0400

    Improve code coverage

--- tests/testthat/test-makeSampleData.R
+++ tests/testthat/test-makeSampleData.R
@@ -1,13 +1,34 @@
 context("makeSampleData")
 
-test_that("makeSampleData", {
-    object <- data.frame(
-        genotype = rep(c("control", "wildtype"), times = 2L),
-        treatment = rep(c("vector", "RNAi"), each = 2L),
-        sampleName = paste("sample", seq_len(4L)),
-        row.names = paste0("GSM000000", seq_len(4L))
+test_that("data.frame with row names", {
+    expect_identical(
+        object = makeSampleData(
+            object = data.frame(
+                "genotype" = rep(c("control", "wildtype"), times = 2L),
+                "treatment" = rep(c("vector", "RNAi"), each = 2L),
+                row.names = paste0("GSM000000", seq_len(4L))
+            )
+        ),
+        expected = DataFrame(
+            "sampleName" = as.factor(paste0("GSM000000", seq_len(4L))),
+            "genotype" = as.factor(rep(c("control", "wildtype"), times = 2L)),
+            "treatment" = as.factor(rep(c("vector", "RNAi"), each = 2L)),
+            row.names = paste0("GSM000000", seq_len(4L))
+        )
     )
-    x <- makeSampleData(object)
-    expect_s4_class(x, "DataFrame")
-    expect_true(all(bapply(x, is.factor)))
+})
+
+test_that("data.frame with 'rowname' column", {
+    expect_identical(
+        object = makeSampleData(
+            object = data.frame(
+                "rowname" = c("sample1", "sample2")
+            )
+        ),
+        expected = DataFrame(
+            "sampleName" = as.factor(c("sample1", "sample2")),
+            "rowname" = as.factor(c("sample1", "sample2")),
+            row.names = c("sample1", "sample2")
+        )
+    )
 })
