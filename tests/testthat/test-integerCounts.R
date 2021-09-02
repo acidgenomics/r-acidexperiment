684d4a4491c859bd7e7724a362cf61abf1bbe9d5
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-02 14:48:53 -0400

    Add code coverage

---
+++ tests/testthat/test-integerCounts.R
@@ -0,0 +1,32 @@
+context("integerCounts")
+
+test_that("SE", {
+    object <- SummarizedExperiment(
+        assays = list(
+            "counts" = matrix(
+                data = c(
+                    0.1, 0.2, 0.3,
+                    1.1, 1.2, 1.3,
+                    2.1, 2.2, 2.3
+                ),
+                nrow = 3L,
+                ncol = 3L,
+                byrow = TRUE
+            )
+        )
+    )
+    expected <- matrix(
+        data = c(
+            0L, 0L, 0L,
+            1L, 1L, 1L,
+            2L, 2L, 2L
+        ),
+        nrow = 3L,
+        ncol = 3L,
+        byrow = TRUE
+    )
+    expect_identical(
+        object = integerCounts(object),
+        expected = expected
+    )
+})
