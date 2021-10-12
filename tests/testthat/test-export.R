36740518210469b86b8c619c6e8eb096aa1d9b79
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-12 12:47:52 -0400

    Improve code coverage of export method

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -1,19 +1,16 @@
-context("export")
+context("export : SE")
 
 testdir <- file.path(tempdir(), "example")
 
-
-## FIXME This is now deprecated.
-test_that("'dir' argument, no 'name'", {
+test_that("New 'con' BiocIO approach, instead of deprecated 'dir'", {
     unlink(testdir, recursive = TRUE)
     object <- rse
     out <- export(
         object = object,
-        name = NULL,
-        dir = testdir,
+        con = testdir,
         compress = TRUE
     )
-    prefix <- realpath(file.path(testdir, "object"))
+    prefix <- realpath(testdir)
     expect_identical(
         object = out,
         expected = list(
@@ -33,11 +30,10 @@ test_that("Compressed sparse matrix support", {
     counts(object) <- as(counts(object), "sparseMatrix")
     out <- export(
         object = object,
-        name = NULL,
-        dir = testdir,
+        con = testdir,
         compress = TRUE
     )
-    prefix <- realpath(file.path(testdir, "object"))
+    prefix <- realpath(testdir)
     expect_identical(
         object = out,
         expected = list(
@@ -67,7 +63,42 @@ test_that("Compressed sparse matrix support", {
     unlink(testdir, recursive = TRUE)
 })
 
-test_that("Both 'name' and 'dir' declared", {
+test_that("Unnamed primary assay", {
+    unlink(testdir, recursive = TRUE)
+    object <- as(rse, "SummarizedExperiment")
+    ## Note that `assayNames()` assignment doesn't work here.
+    names(assays(object)) <- NULL
+    expect_null(assayNames(object))
+    x <- export(object = object, con = testdir)
+    expect_identical(names(x[["assays"]]), "assay")
+    unlink(testdir, recursive = TRUE)
+})
+
+## NOTE This assigns object name nested into directory substructure.
+## This method likely will be removed in a future release.
+test_that("Deprecated : 'dir' argument, no 'name'", {
+    unlink(testdir, recursive = TRUE)
+    object <- rse
+    out <- export(
+        object = object,
+        dir = testdir,
+        compress = TRUE
+    )
+    prefix <- realpath(file.path(testdir, "object"))
+    expect_identical(
+        object = out,
+        expected = list(
+            "assays" = list(
+                "counts" = file.path(prefix, "assays", "counts.csv.gz")
+            ),
+            "colData" = file.path(prefix, "colData.csv.gz"),
+            "rowData" = file.path(prefix, "rowData.csv.gz")
+        )
+    )
+    unlink(testdir, recursive = TRUE)
+})
+
+test_that("Deprecated : both 'name' and 'dir' declared", {
     unlink(testdir, recursive = TRUE)
     object <- rse
     out <- export(
@@ -87,16 +118,5 @@ test_that("Both 'name' and 'dir' declared", {
             "rowData" = file.path(prefix, "rowData.csv")
         )
     )
-    unlink(testdir, recursive = TRUE)
-})
-
-test_that("Unnamed primary assay", {
-    unlink(testdir, recursive = TRUE)
-    object <- as(rse, "SummarizedExperiment")
-    ## Note that `assayNames()` assignment doesn't work here.
-    names(assays(object)) <- NULL
-    expect_null(assayNames(object))
-    x <- export(object = object, dir = testdir)
-    expect_identical(names(x[["assays"]]), "assay")
     unlink(testdir, recursive = TRUE)
 })
