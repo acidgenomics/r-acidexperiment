8d69e55a5bab2ebec9eec005cad9865984bff68d
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-22 12:38:00 -0400

    Draft update

--- R/imports.R
+++ R/imports.R
@@ -22,7 +22,7 @@ NULL
 #' metrics mutateAt nonzeroRowsAndCols removeNA sampleData sampleData<-
 #' selectSamples snakeCase stripGeneVersions stripTranscriptVersions tpm
 #' uniteInterestingGroups upperCamelCase
-#' @importFrom BiocGenerics Map %in% as.data.frame colSums combine counts
+#' @importFrom BiocGenerics Map %in% as.data.frame cbind colSums combine counts
 #' counts<- do.call estimateSizeFactors lapply match organism organism<- rowSums
 #' setdiff sizeFactors sizeFactors<- t unlist unsplit
 #' @importFrom Biobase sampleNames sampleNames<-
