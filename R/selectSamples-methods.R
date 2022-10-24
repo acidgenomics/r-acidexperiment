f4615c1491203d2dde55ea0d169946912da496a1
Michael Steinbaugh <mike@steinbaugh.com>
2022-10-24 14:23:40 -0400

    Convert FIXMEs into TODOs for a future release

--- R/selectSamples-methods.R
+++ R/selectSamples-methods.R
@@ -1,8 +1,3 @@
-## FIXME Unit test input of multiple values.
-## FIXME Error if user passes in a value that's not defined.
-
-
-
 #' @name selectSamples
 #' @inherit AcidGenerics::selectSamples
 #' @note Updated 2022-08-18.
@@ -22,20 +17,12 @@ NULL
 
 
 
-## FIXME Need to tighten our assert check about acceptable input.
-## FIXME Also need to harden to not let the user select based on sampleName
-## or interestingGroups...these are dynamic.
-## FIXME Alternatively, just extract colData and pick the factor columns.
-## We can generalize this to selectColData instead, which is a bit simpler.
-
-
 ## Updated 2022-08-17.
 `selectSamples,SE` <- # nolint
     function(object, ...) {
         args <- list(...)
         assert(
             validObject(object),
-            ## FIXME Consider hardening for scalars here.
             all(vapply(
                 X = args,
                 FUN = is.atomic,
@@ -44,13 +31,9 @@ NULL
             msg = "Arguments must be atomic."
         )
         ## Match the arguments against the sample metadata.
-        ## FIXME Rework using colData and factor selection, to keep this faster.
         sampleData <- sampleData(object)
         assert(isSubset(names(args), colnames(sampleData)))
         ## Obtain the sample identifiers.
-        ## FIXME Work on improving the variable names here, a bit confusing.
-        ## FIXME Need to handle case where input doesn't match anything.
-        ## FIXME What about if user passes in 2 values...both MUST match!
         list <- Map(
             col = names(args),
             arg = args,
