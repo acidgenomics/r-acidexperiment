f2915824eecaecc16ca7965f2018600299759bf7
Michael Steinbaugh <mike@steinbaugh.com>
2023-03-01 14:01:10 -0500

    Update unit test

--- tests/testthat/test-Ensembl2Ncbi.R
+++ tests/testthat/test-Ensembl2Ncbi.R
@@ -1,12 +1,12 @@
 test_that("RangedSummarizedExperiment", {
     formats <- eval(methodFormals(
-        f = "Ensembl2Entrez",
+        f = "Ensembl2Ncbi",
         signature = "GenomicRanges",
         package = "AcidGenomes"
     )[["format"]])
     for (format in formats) {
         object <- rse
-        object <- Ensembl2Entrez(object = object, format = format)
-        expect_s4_class(object, "Ensembl2Entrez")
+        object <- Ensembl2Ncbi(object = object, format = format)
+        expect_s4_class(object, "Ensembl2Ncbi")
     }
 })
