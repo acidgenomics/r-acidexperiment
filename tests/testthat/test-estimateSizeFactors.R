0bb9d5f2a50a57734f63217660fcf6343fff2d8c
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-04 11:42:57 -0400

    Update coverage

--- tests/testthat/test-estimateSizeFactors.R
+++ tests/testthat/test-estimateSizeFactors.R
@@ -1,13 +1,12 @@
 ## NOTE These values can change when we update AcidTest.
 
 test_that("SummarizedExperiment", {
-    for (
-        type in eval(methodFormals(
-            f = "estimateSizeFactors",
-            signature = "SummarizedExperiment",
-            package = "AcidExperiment"
-        )[["type"]])
-    ) {
+    types <- eval(methodFormals(
+        f = "estimateSizeFactors",
+        signature = "SummarizedExperiment",
+        package = "AcidExperiment"
+    )[["type"]])
+    for (type in types) {
         object <- rse
         expect_null(sizeFactors(object))
         object <- estimateSizeFactors(object, type = type)
@@ -18,46 +17,46 @@ test_that("SummarizedExperiment", {
         expected <- switch(
             EXPR = type,
             "mean-ratio" = c(
-                "sample01" = 0.912314,
-                "sample02" = 0.890313,
-                "sample03" = 0.880676,
-                "sample04" = 0.905678,
-                "sample05" = 0.896723,
-                "sample06" = 0.914905,
-                "sample07" = 1.12351,
-                "sample08" = 1.10232,
-                "sample09" = 1.08996,
-                "sample10" = 1.11951,
-                "sample11" = 1.09414,
-                "sample12" = 1.06996
+                "sample01" = 0.888456,
+                "sample02" = 0.887597,
+                "sample03" = 0.883668,
+                "sample04" = 0.903451,
+                "sample05" = 0.904851,
+                "sample06" = 0.908509,
+                "sample07" = 1.12088,
+                "sample08" = 1.09518,
+                "sample09" = 1.08249,
+                "sample10" = 1.12535,
+                "sample11" = 1.08732,
+                "sample12" = 1.11225
             ),
             "geometric-mean-ratio" = c(
-                "sample01" = 0.912314,
-                "sample02" = 0.890313,
-                "sample03" = 0.880676,
-                "sample04" = 0.905678,
-                "sample05" = 0.896723,
-                "sample06" = 0.914905,
-                "sample07" = 1.12351,
-                "sample08" = 1.10232,
-                "sample09" = 1.08996,
-                "sample10" = 1.11951,
-                "sample11" = 1.09414,
-                "sample12" = 1.06996
+                "sample01" = 0.888456,
+                "sample02" = 0.887597,
+                "sample03" = 0.883668,
+                "sample04" = 0.903451,
+                "sample05" = 0.904851,
+                "sample06" = 0.908509,
+                "sample07" = 1.12088,
+                "sample08" = 1.09518,
+                "sample09" = 1.08249,
+                "sample10" = 1.12535,
+                "sample11" = 1.08732,
+                "sample12" = 1.11225
             ),
             "log-geometric-mean-ratio" = c(
-                "sample01" = 0.99133,
-                "sample02" = 0.988888,
-                "sample03" = 0.987799,
-                "sample04" = 0.9906,
-                "sample05" = 0.989605,
-                "sample06" = 0.991614,
-                "sample07" = 1.01217,
-                "sample08" = 1.01026,
-                "sample09" = 1.00913,
-                "sample10" = 1.01181,
-                "sample11" = 1.00952,
-                "sample12" = 1.00728
+                "sample01" = 0.988724,
+                "sample02" = 0.988627,
+                "sample03" = 0.988184,
+                "sample04" = 0.990398,
+                "sample05" = 0.990552,
+                "sample06" = 0.990956,
+                "sample07" = 1.01196,
+                "sample08" = 1.00964,
+                "sample09" = 1.00848,
+                "sample10" = 1.01236,
+                "sample11" = 1.00892,
+                "sample12" = 1.01119
             )
         )
         expect_identical(
