0d01fb8dd2569d314909372e14603cd635796825
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-01 15:05:12 -0500

    Remove legacy format argument

--- tests/testthat/test-EnsemblToNcbi.R
+++ tests/testthat/test-EnsemblToNcbi.R
@@ -1,13 +1,6 @@
 test_that("RangedSummarizedExperiment", {
     rowRanges(rse) <- as(rowRanges(rse), "EnsemblGenes")
-    formats <- eval(methodFormals(
-        f = "EnsemblToNcbi",
-        signature = "EnsemblGenes",
-        package = "AcidGenomes"
-    )[["format"]])
-    for (format in formats) {
-        object <- rse
-        object <- EnsemblToNcbi(object = object, format = format)
-        expect_s4_class(object, "EnsemblToNcbi")
-    }
+    object <- rse
+    object <- EnsemblToNcbi(object)
+    expect_s4_class(object, "EnsemblToNcbi")
 })
